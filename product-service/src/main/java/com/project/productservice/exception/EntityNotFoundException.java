c785431d0932225ebd530578c1742377e9432e7e
Camilla Borges <camillaborgeshelena@gmail.com>
2023-09-20 09:45:01 +0200

    (chore) create the API Gateway component

--- product-service/src/main/java/com/project/productservice/exception/EntityNotFoundException.java
+++ product-service/src/main/java/com/project/productservice/exception/EntityNotFoundException.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class EntityNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
-
-    public EntityNotFoundException() {
-        super();
-    }
+    
 
     public EntityNotFoundException(String message) {
         super(message);
